c8bf727ad64569078e3d7325c8869a8b91ac7217
islee2006 <islee.lee@mail.utoronto.ca>
2026-03-19 14:47:30 -0400

    movement (no double buffer) draft 2

--- movement.c
+++ movement.c
@@ -1,113 +1,85 @@
-#include <stdlib.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
 #define PS2_BASE 0xFF200100
 
-volatile int pixel_buffer_start;
-volatile int *pixel_ctrl_ptr = (int *)0xFF203020;
-volatile int *PS2_ptr = (int *)PS2_BASE;
-
-short int Buffer1[240][512];
-short int Buffer2[240][512];
-
-struct box {
-    short color;
-    int x, y;
-    int oldx1, oldy1;
-};
+int pixel_buffer_start;
 
 void plot_pixel(int x, int y, short int color);
-void draw_box(int x, int y, short int color);
-void clear_screen(void);
-void wait_for_vsync(void);
+void draw_rectangle(int x0, int y0, int x1, int y1, short int color);
+void draw_player(int x, int y, short int color);
 
-void wait_for_vsync(void) {
-    *pixel_ctrl_ptr = 1;
-    while ((*(pixel_ctrl_ptr + 3) & 0x00000001) != 0) {
-    }
+void plot_pixel(int x, int y, short int color)
+{
+    volatile short int *one_pixel_address;
+    one_pixel_address = (volatile short int *)(pixel_buffer_start + (y << 10) + (x << 1));
+    *one_pixel_address = color;
 }
-void clear_screen(void) {
-    int i, j;
-    for (i = 0; i < 320; i++) {
-        for (j = 0; j < 240; j++) {
-            plot_pixel(i, j, 0x0000);
+
+void draw_rectangle(int x0, int y0, int x1, int y1, short int color)
+{
+    int x, y;
+    for (x = x0; x <= x1; x++) {
+        for (y = y0; y <= y1; y++) {
+            plot_pixel(x, y, color);
         }
     }
 }
 
-int main(void) {
-    struct box character;
+void draw_player(int x, int y, short int color)
+{
+    draw_rectangle(x, y, x + 1, y + 1, color);
+}
+
+int main(void)
+{
+    volatile int *pixel_ctrl_ptr = (int *)0xFF203020;
+    volatile int *PS2_ptr = (int *)PS2_BASE;
+
+    int x = 10;
+    int y = 120;
 
     int PS2_data;
     int RVALID;
     char byte;
 
-    character.x = 100;
-    character.y = 100;
-    character.oldx1 = 100;
-    character.oldy1 = 100;
-    character.color = 0xFFFF;
-
-    *(pixel_ctrl_ptr + 1) = (int)&Buffer1;
-    wait_for_vsync();
     pixel_buffer_start = *pixel_ctrl_ptr;
-    clear_screen();
 
-    *(pixel_ctrl_ptr + 1) = (int)&Buffer2;
-    pixel_buffer_start = *(pixel_ctrl_ptr + 1);
-    clear_screen();
-
-    draw_box(character.x, character.y, character.color);
+    draw_player(x, y, 0xFFFF);
 
     while (1) {
-        character.oldx1 = character.x;
-        character.oldy1 = character.y;
-
         PS2_data = *PS2_ptr;
         RVALID = PS2_data & 0x8000;
 
         if (RVALID) {
             byte = PS2_data & 0xFF;
 
-            if (byte == 0x1D) {          // W
-                if (character.y > 0) {
-                    character.y--;
+            draw_player(x, y, 0x0000);
+
+            if (byte == 0x1D) {          //W
+                if (y > 0) {             //Second if statement contains the boundary condition for movement
+                    y--;
                 }
             }
-            else if (byte == 0x1C) {     // A
-                if (character.x > 0) {
-                    character.x--;
+            else if (byte == 0x1C) {     //A
+                if (x > 0) {
+                    x--;
                 }
             }
-            else if (byte == 0x1B) {     // S
-                if (character.y < 238) {
-                    character.y++;
+            else if (byte == 0x1B) {     //S
+                if (y < 238) {
+                    y++;
                 }
             }
-            else if (byte == 0x23) {     // D
-                if (character.x < 318) {
-                    character.x++;
+            else if (byte == 0x23) {     //D
+                if (x < 318) {
+                    x++;
                 }
             }
+
+            draw_player(x, y, 0xFFFF);
         }
-
-        draw_box(character.oldx1, character.oldy1, 0x0000);
-        draw_box(character.x, character.y, character.color);
-
-        wait_for_vsync();
-        pixel_buffer_start = *(pixel_ctrl_ptr + 1);
     }
-}
 
-void plot_pixel(int x, int y, short int color) {
-    volatile short int *one_pixel_address;
-    one_pixel_address = (volatile short int *)(pixel_buffer_start + (y << 10) + (x << 1));
-    *one_pixel_address = color;
-}
-
-void draw_box(int x, int y, short int color) {
-    plot_pixel(x, y, color);
-    plot_pixel(x + 1, y, color);
-    plot_pixel(x, y + 1, color);
-    plot_pixel(x + 1, y + 1, color);
+    return 0;
 }
