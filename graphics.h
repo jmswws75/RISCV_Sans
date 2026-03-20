0370f492a0c608db45324624340349168e9bbf60
James <jmswws75@gmail.com>
2026-03-20 00:25:00 -0400

    2nd attempt at multiple file compilation

---
+++ graphics.h
@@ -0,0 +1,28 @@
+#ifndef GRAPHICS_H
+#define GRAPHICS_H
+
+#define GASTER_BLASTER_HEIGHT 56
+#define GASTER_BLASTER_WIDTH 22
+
+struct Bone {
+    // the positions and velocities are stored as multiples of 256 (2^8).
+    // i.e. x = 256 means x = 1. same for velocities.
+    // this is to make the calculation easier for the cpu. (avoid floating point arithmatic)
+    int posx[3];
+    int posy[3];
+    int length;
+    int velox;
+    int veloy;
+    short int color;
+};
+
+void wait_for_vsync();
+void plot_pixel(int x, int y, short int line_color);
+void update_pos(int newPos, int pos[]);
+void draw_blaster();
+void draw_bone(int x0, int y0, int length, short int color, int bounds[4]);
+void draw_line(int x0, int y0, int x1, int y1, int color);
+void draw_rectangle(int x0, int y0, int x1, int y1, short int color, int bounds[4]);
+void clear_screen();
+
+#endif
