50f806ddf50290702ab1677ff6c6df5358126979
islee2006 <islee.lee@mail.utoronto.ca>
2026-03-20 16:01:01 -0400

    movement draft 3

--- movement.c
+++ movement.c
@@ -3,9 +3,12 @@
 
 #define PS2_BASE 0xFF200100
 
-int pixel_buffer_start;
+volatile int pixel_buffer_start;
+volatile int *pixel_ctrl_ptr = (int *)0xFF203020;
+volatile int *PS2_ptr = (int *)PS2_BASE;
 
 void plot_pixel(int x, int y, short int color);
+void clear_screen(void);
 void draw_rectangle(int x0, int y0, int x1, int y1, short int color);
 void draw_player(int x, int y, short int color);
 
@@ -14,6 +17,16 @@ void plot_pixel(int x, int y, short int color)
     volatile short int *one_pixel_address;
     one_pixel_address = (volatile short int *)(pixel_buffer_start + (y << 10) + (x << 1));
     *one_pixel_address = color;
+}
+
+void clear_screen(void)
+{
+    int x, y;
+    for (x = 0; x < 320; x++) {
+        for (y = 0; y < 240; y++) {
+            plot_pixel(x, y, 0x0000);
+        }
+    }
 }
 
 void draw_rectangle(int x0, int y0, int x1, int y1, short int color)
@@ -33,50 +46,53 @@ void draw_player(int x, int y, short int color)
 
 int main(void)
 {
-    volatile int *pixel_ctrl_ptr = (int *)0xFF203020;
-    volatile int *PS2_ptr = (int *)PS2_BASE;
-
     int x = 10;
     int y = 120;
+    int old_x = x;
+    int old_y = y;
 
     int PS2_data;
     int RVALID;
     char byte;
+    char last_byte = 0;
 
     pixel_buffer_start = *pixel_ctrl_ptr;
-
+    clear_screen();
     draw_player(x, y, 0xFFFF);
 
     while (1) {
-        PS2_data = *PS2_ptr;
-        RVALID = PS2_data & 0x8000;
+        old_x = x;
+        old_y = y;
 
-        if (RVALID) {
+        while (1) {
+            PS2_data = *PS2_ptr;
+            RVALID = PS2_data & 0x8000;
+            if (!RVALID){
+                break;
+			}
+
             byte = PS2_data & 0xFF;
 
-            draw_player(x, y, 0x0000);
-
-            if (byte == 0x1D) {          //W
-                if (y > 0) {             //Second if statement contains the boundary condition for movement
-                    y--;
+            if (byte != (char)0xF0 && last_byte != (char)0xF0) {
+                if (byte == (char)0x1D) {
+                    if (y > 0) y--;
                 }
-            }
-            else if (byte == 0x1C) {     //A
-                if (x > 0) {
-                    x--;
+                else if (byte == (char)0x1C) {
+                    if (x > 0) x--;
                 }
-            }
-            else if (byte == 0x1B) {     //S
-                if (y < 238) {
-                    y++;
+                else if (byte == (char)0x1B) {
+                    if (y < 238) y++;
                 }
-            }
-            else if (byte == 0x23) {     //D
-                if (x < 318) {
-                    x++;
+                else if (byte == (char)0x23) {
+                    if (x < 318) x++;
                 }
             }
 
+            last_byte = byte;
+        }
+
+        if (x != old_x || y != old_y) {
+            draw_player(old_x, old_y, 0x0000);
             draw_player(x, y, 0xFFFF);
         }
     }
