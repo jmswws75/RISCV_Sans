f7be1b0bf8d9f700e16e0aebd1e415aa0a5cb4c0
jmswws75 <62361325+jmswws75@users.noreply.github.com>
2026-03-29 22:35:25 -0400

    Update stage_9.c

--- stage_9.c
+++ stage_9.c
@@ -1,6 +1,7 @@
-//#include "graphics.h"
+#include "graphics.h"
 #include <stdlib.h>
 #include <stdbool.h>
+#include "movement.h"
 
 void swap_buffers(void);
 
@@ -15,12 +16,29 @@ void swap_buffers(void);
 #define BLASTER_SHOOT_LEFT 90
 #define BLASTER_OFFSET 40
 
-void run_stage_9(void) {
+int run_stage_9(int *Global_health) {
     struct platform plats[NUM_PLATFORMS];
     struct blaster active_blaster;
 
     int bounds_default[4] = {70, 129, 251, 192};
     int bounds_unlimited[4] = {0, 0, 319, 239};
+	
+	struct player player1;
+    player1.ground = (192 - 5) << 8;
+    for (int i = 0; i < 3; i++) {
+        player1.posx[i] = 157 << 8;
+        player1.posy[i] = 150 << 8;
+    }
+    player1.was_up_pressed = false;
+    player1.have_gravity = true;
+    
+    for (int i = 0; i< 4; i++) {
+        player1.bounds[i] = bounds_default[i];
+    }
+    player1.health = *Global_health;
+    player1.gravity = 8;
+    player1.veloY = 0;
+    player1.burst_force = 400;
     
     const int left_border = bounds_default[0];
     const int top_border = bounds_default[1];
@@ -80,9 +98,14 @@ void run_stage_9(void) {
     bool blaster_just_finished = false;
 
     while (1) {
+		for (int i = 0; i < NUM_PLATFORMS; i++) {
+			erase_platform(&plats[i], 2, bounds_default);
+		}
+		
+		draw_player(&player1, 1, 0x0000);
+		
         for (int i = 0; i < NUM_PLATFORMS; i++) {
             update_platform(&plats[i]);
-            erase_platform(&plats[i], 2, bounds_default);
             draw_platform(&plats[i], 0, bounds_default);
         }
 
@@ -124,6 +147,13 @@ void run_stage_9(void) {
         draw_rectangle(67, 126, 69, 195, 0xFFFF, bounds_unlimited);
         draw_rectangle(67, 193, 254, 195, 0xFFFF, bounds_unlimited);
         draw_rectangle(252, 126, 254, 195, 0xFFFF, bounds_unlimited);
+		
+		movement(&player1, &plats, NUM_PLATFORMS);
+        draw_rectangle(0,0, 30, 20, 0x0000, bounds_unlimited);
+        draw_number(0,0,player1.health/10);
+        draw_number(12,0,player1.health%10);
+        draw_player(&player1, 0, 0xf800);
+        if (player1.health == 0) {return 1;}
 
         swap_buffers();
 
@@ -131,5 +161,43 @@ void run_stage_9(void) {
         if (!blaster_active && blasters_spawned >= TOTAL_BLASTERS) {
             break;
         }
+    }
+	
+	// interstage part
+
+    *Global_health = player1.health;
+
+    for (int i = 0; i< 4; i++) {
+        player1.bounds[i] = bounds_unlimited[i];
+    }
+
+    clear_screen();
+    swap_buffers();
+    clear_screen();
+
+    update_pos(160 << 8, player1.posx);
+    update_pos(120 << 8, player1.posy);
+
+    while (1) {
+
+        draw_player(&player1, 1, 0x0000);
+        int result = interstage_movement(&player1);
+        draw_player(&player1, 0, 0xf800);
+        draw_rectangle(0,0, 30, 20, 0x0000, bounds_unlimited);
+        draw_number(0,0,player1.health/10);
+        draw_number(12,0,player1.health%10);
+
+        draw_fight_button(100, 180);
+        draw_iteam_button(200, 180);
+
+        if (result == 2) {
+            return 2; // player chooses to fight
+        } else if (result == 3) {
+            *Global_health += 99;
+            if (*Global_health > 99) {*Global_health = 99;}
+            return 3; // player chooses to heal
+        }
+
+        swap_buffers();
     }
 }
